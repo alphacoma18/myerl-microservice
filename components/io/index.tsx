4e5c9d64591d3b1397ccd69fc1d08784a0348462
mnhs-shs <general.bakudan@gmail.com>
2022-08-22 23:37:11 +0800

    Added axios

--- components/io/index.tsx
+++ components/io/index.tsx
@@ -2,10 +2,21 @@ import React, { useState } from "react";
 import styles from "./index.module.css";
 const IO: React.FC = () => {
 	const [urlInput, setUrlInput] = useState<string>("");
+	const [error, setError] = useState<string>("");
+	const regex =
+		/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+	function handleSubmit() {
+		try {
+			const pass = regex.test(urlInput);
+			if (!pass) throw "Error: Invalid URL";
+		} catch (err: any) {
+			setError(err.message);
+		}
+	}
 
 	return (
 		<>
-			<form className={styles.inputDiv}>
+			<form className={styles.inputDiv} onSubmit={handleSubmit}>
 				<input
 					type="text"
 					className={styles.generateURLInput}
--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,5 +1,13 @@
-import {NextApiRequest, NextApiResponse} from "next"
+import ERL from "../../utils/db/index";
+import { NextApiRequest, NextApiResponse } from "next";
 async function ApiHandler(req: NextApiRequest, res: NextApiResponse) {
-
-};
+	try {
+		if (req.method === "POST") {
+			const originalURL = req.body.url;
+			if (!originalURL) throw "Error: Invalid URL";
+		}
+	} catch (err: any) {
+		return res.json({ err: err.message });
+	}
+}
 export default ApiHandler;
