92005aa09a9a7f6b94c395213c3380bdd99b114f
mnhs-shs <general.bakudan@gmail.com>
2022-08-25 00:10:53 +0800

    Fixed wrongly put web worker

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const withPWA = require("next-pwa");
-const prod = process.env.NODE_ENV === "production";
-const nextConfig = withPWA({
-	reactStrictMode: true,
-	pwa: {
-		dest: "public",
-		disable: prod ? false : true,
-		register: true,
-		skipWaiting: true,
-		disable: process.env.NODE_ENV === "development",
-	},
-});
-
-module.exports = nextConfig;
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+const withPWA = require("next-pwa");
+const prod = process.env.NODE_ENV === "production";
+const nextConfig = withPWA({
+	reactStrictMode: true,
+	pwa: {
+		dest: "public",
+		disable: prod ? false : true,
+		register: true,
+		skipWaiting: true,
+		disable: process.env.NODE_ENV === "development",
+	},
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
