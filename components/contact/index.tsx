6f623e5a5826b3d1dcfea50fee084a37a2540c4e
mnhs-shs <general.bakudan@gmail.com>
2022-08-24 16:39:43 +0800

    Added contact us

--- components/contact/index.tsx
+++ components/contact/index.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import styles from "./index.module.css";
 const Contact: React.FC = () => {
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		try {
+			e.preventDefault();
+			console.log("Hello");
+		} catch (err) {}
+	}
 	return (
 		<>
 			<div className={styles.flexContact}>
@@ -8,22 +14,38 @@ const Contact: React.FC = () => {
 					Contact Us
 				</h1>
 				<div className={styles.itemContact}>
-					<form action="post">
+					<h1>Hello World</h1>
+				</div>
+				<div className={styles.itemContact}>
+					<form
+						action="post"
+						className={styles.flexFormInput}
+						onSubmit={handleSubmit}
+					>
+						<h3>Have a message for us? Send us an email!</h3>
 						<input
 							className={styles.itemFormInput}
 							type="text"
-							minLength={4}
 							required
+							minLength={10}
+							placeholder=">>> Your Name"
+							autoComplete="off"
 						/>
 						<input
 							className={styles.itemFormInput}
 							type="email"
-							minLength={10}
 							required
+							minLength={10}
+							placeholder=">>> Your Google Email"
+							autoComplete="off"
+							pattern="([a-zA-Z0-9]+)([.{1}])?([a-zA-Z0-9]+)@(?:gmail|GMAIL)([.])(?:com|COM)"
+							title=">>> Your Google Email"
 						/>
 						<textarea
 							className={styles.itemFormInput}
 							required
+							minLength={25}
+							placeholder=">>> Your Message"
 						></textarea>
 					</form>
 				</div>
