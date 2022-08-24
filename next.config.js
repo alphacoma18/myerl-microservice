c8e07147605d7380818a3b9265cfbd13c5ef99eb
mnhs-shs <general.bakudan@gmail.com>
2022-08-25 00:27:40 +0800

    Web worker w/ manifest now working

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const withPWA = require("next-pwa");
-const prod = process.env.NODE_ENV === "production";
-const nextConfig = withPWA({
-	reactStrictMode: true,
-	pwa: {
-		dest: "public",
-		disable: prod ? false : true,
-		register: true,
-		skipWaiting: true,
-		disable: process.env.NODE_ENV === "development",
-	},
+/** @type {import('next').NextConfig} */
+const withPWA = require("next-pwa")({
+	dest: "public",
+	disable: process.env.NODE_ENV === "development",
+	register: true,
+	scope: "/app",
+	sw: "service-worker.js",
 });
 
-module.exports = nextConfig;
+module.exports = withPWA({});
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
 					sizes="16x16"
 					href="/favicon-16x16.png"
 				/>
-				<link rel="manifest" href="/site.webmanifest" />
+				<link rel="manifest" href="/manifest.webmanifest" />
 				<link
 					rel="mask-icon"
 					href="/safari-pinned-tab.svg"
