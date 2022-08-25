09f08a3bf478f8678cb2e9d8ed2d39535545f4d3
mnhs-shs <general.bakudan@gmail.com>
2022-08-25 19:10:00 +0800

    Nav bar now responsive

--- components/nav/index.tsx
+++ components/nav/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import styles from "./index.module.css";
 import Image from "next/image";
 const Nav: React.FC = () => {
+	const [isToggled, setIsToggled] = useState<boolean>(false);
 	return (
 		<nav className={styles.outermostNav}>
 			<div className={styles.myerlLogo}>
@@ -19,45 +20,86 @@ const Nav: React.FC = () => {
 					/>
 				</a>
 			</div>
-			<div className={styles.navLinkHolder}>
-				<Link href="/#link-home">
-					<a className={styles.navLink}>
+			<div className={styles.myerlMobile}>
+				<Image
+					src={"/logo/logo.png"}
+					width={60}
+					height={60}
+					onClick={() => setIsToggled((prev) => !prev)}
+				/>
+			</div>
+			<div
+				className={
+					isToggled
+						? styles.navLinkHolderToggled
+						: styles.navLinkHolder
+				}
+			>
+				<a
+					className={styles.closeMenu}
+					onClick={() => setIsToggled((prev) => !prev)}
+				>
+					&times;
+				</a>
+				<Link href="/">
+					<a
+						className={styles.navLink}
+						onClick={() => setIsToggled((prev) => !prev)}
+					>
 						<i className="icon-expeditedssl"></i>
 						<span className={styles.linkSpan}>Password</span>
 					</a>
 				</Link>
 				<Link href="/#link-home">
-					<a className={styles.navLink}>
+					<a
+						className={styles.navLink}
+						onClick={() => setIsToggled((prev) => !prev)}
+					>
 						<i className="icon-home"></i>
 						<span className={styles.linkSpan}>Home</span>
 					</a>
 				</Link>
 				<Link href="/#link-about">
-					<a className={styles.navLink}>
+					<a
+						className={styles.navLink}
+						onClick={() => setIsToggled((prev) => !prev)}
+					>
 						<i className="icon-bank"></i>
 						<span className={styles.linkSpan}>About</span>
 					</a>
 				</Link>
 				<Link href="/#link-faqs">
-					<a className={styles.navLink}>
+					<a
+						className={styles.navLink}
+						onClick={() => setIsToggled((prev) => !prev)}
+					>
 						<i className="icon-info-circled"></i>
 						<span className={styles.linkSpan}>FAQs</span>
 					</a>
 				</Link>
 				<Link href="/#link-terms">
-					<a className={styles.navLink}>
+					<a
+						className={styles.navLink}
+						onClick={() => setIsToggled((prev) => !prev)}
+					>
 						<i className="icon-doc-text-inv"></i>
 						<span className={styles.linkSpan}>T&Cs</span>
 					</a>
 				</Link>
 				<Link href="/#link-api">
-					<a className={styles.navLink}>
+					<a
+						className={styles.navLink}
+						onClick={() => setIsToggled((prev) => !prev)}
+					>
 						<i className="icon-code"></i>
 						<span className={styles.linkSpan}>API</span>
 					</a>
 				</Link>
 				<Link href="/#link-contact">
-					<a className={styles.navLink}>
+					<a
+						className={styles.navLink}
+						onClick={() => setIsToggled((prev) => !prev)}
+					>
 						<i className="icon-mail-alt"></i>
 						<span className={styles.linkSpan}>Contact</span>
 					</a>
